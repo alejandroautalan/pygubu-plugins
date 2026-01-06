f60e95ca3a35634374f55de9567f0024f862dcbd
Alejandro Autalan <alejandroautalan@gmail.com>
2026-01-06 05:16:22 -0300

    Add more properties.

--- packages/pygubu-ttkbootstrap/src/pygubu_ttkbootstrap/designer/properties.py
+++ packages/pygubu-ttkbootstrap/src/pygubu_ttkbootstrap/designer/properties.py
@@ -1,31 +1,45 @@
 from pygubu.api.v1 import register_custom_property
-from ..config import namespace, windows  # primitives
+from ..config import namespace, windows, primitives
 
 _builder_all = f"{namespace}.*"
 
-plugin_properties = {
-    "color": dict(
+plugin_properties = dict(
+    color=dict(
         buid=_builder_all,
         editor="choice",
         values=("", "primary", "danger", "success"),
         state="readonly",
     ),
-    "variant": dict(
-        buid=_builder_all,
-        editor="choice",
-        values=("", "default", "inverse"),
-        state="readonly",
-    ),
-    "icon": dict(buid=_builder_all),
-    "maxsize": dict(buid=[windows.app, windows.toplevel], editor="whentry"),
-    "minsize": dict(buid=[windows.app, windows.toplevel], editor="whentry"),
-    "resizable": dict(
+    icon=dict(buid=_builder_all),
+    maxsize=dict(buid=[windows.app, windows.toplevel], editor="whentry"),
+    minsize=dict(buid=[windows.app, windows.toplevel], editor="whentry"),
+    resizable=dict(
         buid=[windows.app, windows.toplevel],
         editor="choice",
         values=("", "both", "horizontally", "vertically", "none"),
         state="readonly",
     ),
-}
+    state=[
+        dict(
+            buid=[_builder_all],  # FIXME: for all widgets?
+            editor="choice",
+            values=("", "normal", "active", "disabled", "readonly"),
+            state="readonly",
+        ),
+        dict(
+            buid=[primitives.combobox, primitives.combobox],
+            editor="choice",
+            values=("", "normal", "disabled", "readonly"),
+            state="readonly",
+        ),
+    ],
+    variant=dict(
+        buid=_builder_all,
+        editor="choice",
+        values=("", "default", "inverse"),
+        state="readonly",
+    ),
+)
 
 for prop in plugin_properties:
     definitions = plugin_properties[prop]
--- packages/pygubu-ttkbootstrap/src/pygubu_ttkbootstrap/primitives.py
+++ packages/pygubu-ttkbootstrap/src/pygubu_ttkbootstrap/primitives.py
@@ -1,3 +1,4 @@
+from tkinter import getboolean
 from pygubu.api.v1 import BuilderObject, register_widget
 from ttkbootstrap import (
     Badge,
@@ -35,7 +36,19 @@ from pygubu.plugins.ttk.ttkstdwidgets import (
 from .config import primitives, primitives_ns, tab_group
 
 
-class LabelBO(BuilderObject):
+class ttkbWidgetMixin:
+    def _process_property_value(self, pname, value):
+        if pname == "localize":
+            return getboolean(value)
+        return super()._process_property_value(pname, value)
+
+    def _code_process_property_value(self, targetid, pname, value):
+        if pname == "localize":
+            return self._process_property_value(pname, value)
+        return super()._code_process_property_value(targetid, pname, value)
+
+
+class LabelBO(ttkbWidgetMixin, BuilderObject):
     class_ = Label
     properties = (
         "class_",
@@ -62,6 +75,7 @@ class LabelBO(BuilderObject):
         "style",
         "color",
         "variant",
+        "state",
     )
     ro_properties = ("color",)  # FIXME
 
@@ -80,9 +94,33 @@ register_widget(
 )
 
 
-class ButtonBO(BuilderObject):
+class ButtonBO(ttkbWidgetMixin, BuilderObject):
     class_ = Button
-    properties = ("text",)
+    properties = (
+        "text",
+        "command",
+        "image",
+        "icon",
+        "icon_only",
+        "anchor",
+        "compound",
+        "padding",
+        "width",
+        "underline",
+        "state",
+        "takefocus",
+        "localize",
+        "style",
+        "class_",
+        "cursor",
+        "default",
+        # "name",
+        "textvariable",
+        # "color",
+        # "variant",
+    )
+    ro_properties = properties
+    command_properties = ("command",)
 
 
 register_widget(
@@ -90,9 +128,39 @@ register_widget(
 )
 
 
-class CheckButtonBO(BuilderObject):
+class CheckButtonBO(ttkbWidgetMixin, BuilderObject):
     class_ = CheckButton
-    properties = ("text",)
+    properties = (
+        "text",
+        "command",
+        "image",
+        "icon",
+        "icon_only",
+        "compound",
+        "variable",
+        # "signal",
+        "value",
+        "onvalue",
+        "offvalue",
+        "padding",
+        "anchor",
+        "width",
+        "underline",
+        "state",
+        "takefocus",
+        "style",
+        "class_",
+        "cur",
+        # "name",
+        "textvariable",
+        # "textsignal",
+        # "color",
+        # "vari",
+        # "surface_color",
+        "localize",
+    )
+    ro_properties = properties
+    command_properties = ("command",)
 
 
 register_widget(
@@ -104,9 +172,8 @@ register_widget(
 )
 
 
-class CheckToggleBO(BuilderObject):
+class CheckToggleBO(CheckButtonBO):
     class_ = CheckToggle
-    properties = ("text",)
 
 
 register_widget(
@@ -118,9 +185,95 @@ register_widget(
 )
 
 
-class ComboboxBO(BuilderObject):
+class SwitchBO(CheckButtonBO):
+    class_ = Switch
+
+
+register_widget(
+    primitives.switch, SwitchBO, "Switch", tab_group, group=primitives_ns.switch
+)
+
+
+class RadioButtonBO(ttkbWidgetMixin, BuilderObject):
+    class_ = RadioButton
+    properties = (
+        "text",
+        "command",
+        "image",
+        "icon",
+        "icon_only",
+        "compound",
+        "variable",
+        # "signal",
+        "value",
+        "padding",
+        "anchor",
+        "width",
+        "underline",
+        "state",
+        "takefocus",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        "textvariable",
+        # "textsignal",
+        # "color",
+        # "variant",
+        # "surface_color",
+        "localize",
+    )
+    ro_properties = properties
+    command_properties = ("command",)
+
+
+register_widget(
+    primitives.radiobutton,
+    RadioButtonBO,
+    "RadioButton",
+    tab_group,
+    group=primitives_ns.radiobutton,
+)
+
+
+class RadioToggleBO(RadioButtonBO):
+    class_ = RadioToggle
+
+
+register_widget(
+    primitives.radiotoggle,
+    RadioToggleBO,
+    "RadioToggle",
+    tab_group,
+    group=primitives_ns.radiotoggle,
+)
+
+
+class ComboboxBO(ttkbWidgetMixin, BuilderObject):
     class_ = Combobox
-    properties = tuple()
+    properties = (
+        "values",
+        "textvariable",
+        "textsignal",
+        "state",
+        "width",
+        "height",
+        "postcommand",
+        "justify",
+        "exportselection",
+        "xscrollcommand",
+        "font",
+        "foreground",
+        "background",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "surface_color",
+    )
+    ro_properties = properties
+    command_properties = ("postcommand",)
 
 
 register_widget(
@@ -128,9 +281,34 @@ register_widget(
 )
 
 
-class EntryBO(BuilderObject):
+class EntryBO(ttkbWidgetMixin, BuilderObject):
     class_ = Entry
-    properties = []
+    properties = (
+        "textvariable",
+        # "textsignal",
+        "show",
+        "width",
+        "exportselection",
+        "justify",
+        "validate",
+        "validatecommand",
+        "invalidcommand",
+        "xscrollcommand",
+        "font",
+        "foreground",
+        "background",
+        "state",
+        "takefocus",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "variant",
+        # "surface_color",
+    )
+    ro_properties = properties
+    command_properties = ("validatecommand", "invalidcommand", "xscrollcommand")
 
 
 register_widget(
@@ -138,7 +316,7 @@ register_widget(
 )
 
 
-class FrameBO(BuilderObject):
+class FrameBO(ttkbWidgetMixin, BuilderObject):
     class_ = Frame
     container = True
     container_layout = True
@@ -152,7 +330,12 @@ class FrameBO(BuilderObject):
         "padding",
         "height",
         "width",
+        "takefocus",
+        # "color",
+        # "variant",
+        "show_border",
     )
+    ro_properties = properties
 
 
 register_widget(
@@ -160,11 +343,10 @@ register_widget(
 )
 
 
-class GridFrameBO(BuilderObject):
+class GridFrameBO(FrameBO):
     class_ = GridFrame
     container = True
-    container_layout = True
-    properties = ("class_",)
+    container_layout = False  # FIXME: howto handle this widget?
 
 
 register_widget(
@@ -176,14 +358,42 @@ register_widget(
 )
 
 
-class LabelFrameBO(BuilderObject):
+class PackFrameBO(FrameBO):
+    class_ = PackFrame
+    container = True
+    container_layout = False  # FIXME: howto handle this widget?
+
+
+register_widget(
+    primitives.packframe,
+    PackFrameBO,
+    "PackFrame",
+    tab_group,
+    group=primitives_ns.packframe,
+)
+
+
+class LabelFrameBO(ttkbWidgetMixin, BuilderObject):
     class_ = LabelFrame
     container = True
     container_layout = True
     properties = (
         "text",
+        "labelanchor",
         "padding",
+        "relief",
+        "borderwidth",
+        "width",
+        "height",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "surface_color",
+        "localize",
     )
+    ro_properties = properties
 
 
 register_widget(
@@ -195,9 +405,31 @@ register_widget(
 )
 
 
-class MenuButtonBO(BuilderObject):
+class MenuButtonBO(ttkbWidgetMixin, BuilderObject):
     class_ = MenuButton
-    properties = ("text",)
+    properties = (
+        "text",
+        "image",
+        "icon",
+        "icon_only",
+        "compound",
+        "direction",
+        "menu",
+        "padding",
+        "state",
+        "takefocus",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        "textvariable",
+        # "textsignal",
+        # "color",
+        # "variant",
+        # "surface_color",
+        "localize",
+    )
+    ro_properties = properties
 
 
 register_widget(
@@ -209,21 +441,32 @@ register_widget(
 )
 
 
-class NotebookBO(BuilderObject):
+class NotebookBO(ttkbWidgetMixin, BuilderObject):
     class_ = Notebook
     container = True
     allowed_children = (primitives.notebook_tab,)
+    virtual_events = (
+        "<<NotebookTabChange>>",
+        "<<NotebookTabActivate>>",
+        "<<NotebookTabDeactivate>>",
+    )
     properties = (
         "padding",
+        "height",
         "width",
+        "style",
+        "class_",
+        "cursor",
+        # "color",
+        # "variant",
+        # "surface_color",
     )
+    ro_properties = properties
 
 
 register_widget(
     primitives.notebook, NotebookBO, "Notebook", tab_group, group=primitives_ns.notebook
 )
-
-Progressbar
 
 
 class NotebookTabBO(TTKNotebookTab): ...
@@ -240,9 +483,34 @@ register_widget(
 NotebookTabBO.add_allowed_parent(primitives.notebook)
 
 
-class OptionMenuBO(BuilderObject):
+class OptionMenuBO(ttkbWidgetMixin, BuilderObject):
     class_ = OptionMenu
-    properties = ("padding",)
+    properties = (
+        "command",
+        "image",
+        "icon",
+        "icon_only",
+        "compound",
+        "padding",
+        "width",
+        "underline",
+        "state",
+        "takefocus",
+        "style",
+        "class_",
+        "cursor",
+        "default",
+        # "name",
+        "textvariable",
+        # "textsignal",
+        # "color",
+        # "variant",
+        # "surface_color",
+        "show_dropdown_button",
+        "dropdown_button_icon",
+    )
+    ro_properties = properties
+    command_properties = ("command",)
 
 
 register_widget(
@@ -254,28 +522,24 @@ register_widget(
 )
 
 
-class PackFrameBO(BuilderObject):
-    class_ = PackFrame
-    container = True
-    container_layout = False
-    properties = ("padding",)
-
-
-register_widget(
-    primitives.packframe,
-    PackFrameBO,
-    "PackFrame",
-    tab_group,
-    group=primitives_ns.packframe,
-)
-
-
-class PanedWindowBO(BuilderObject):
+class PanedWindowBO(ttkbWidgetMixin, BuilderObject):
     class_ = PanedWindow
     container = True
     allowed_children = (primitives.panedwindow_pane,)
-    properties = ("orient",)
-    ro_properties = ("orient",)
+    properties = (
+        "orient",
+        "padding",
+        "width",
+        "height",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "variant",
+        # "surface_color",
+    )
+    ro_properties = properties
 
 
 register_widget(
@@ -301,12 +565,26 @@ register_widget(
 PanedWindowPaneBO.add_allowed_parent(primitives.panedwindow)
 
 
-class ProgressbarBO(BuilderObject):
+class ProgressbarBO(ttkbWidgetMixin, BuilderObject):
     class_ = Progressbar
     properties = (
         "mode",
         "orient",
+        "length",
+        "maximum",
+        "value",
+        "variable",
+        # "signal",  FIXME !
+        "phase",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "variant",
+        # "surface_color",
     )
+    ro_properties = properties
 
 
 register_widget(
@@ -318,40 +596,28 @@ register_widget(
 )
 
 
-class RadioButtonBO(BuilderObject):
-    class_ = RadioButton
-    properties = ("text",)
-
-
-register_widget(
-    primitives.radiobutton,
-    RadioButtonBO,
-    "RadioButton",
-    tab_group,
-    group=primitives_ns.radiobutton,
-)
-
-
-class RadioToggleBO(RadioButtonBO):
-    class_ = RadioToggle
-
-
-register_widget(
-    primitives.radiotoggle,
-    RadioToggleBO,
-    "RadioToggle",
-    tab_group,
-    group=primitives_ns.radiotoggle,
-)
-
-
-class ScaleBO(BuilderObject):
+class ScaleBO(ttkbWidgetMixin, BuilderObject):
     class_ = Scale
     properties = (
-        "orient",
         "from_",
         "to",
+        "value",
+        "variable",
+        # "signal",
+        "orient",
+        "length",
+        "command",
+        "takefocus",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "variant",
+        # "surface_color",
     )
+    ro_properties = properties
+    command_properties = ("command",)
 
 
 register_widget(
@@ -359,10 +625,21 @@ register_widget(
 )
 
 
-class ScrollbarBO(BuilderObject):
+class ScrollbarBO(ttkbWidgetMixin, BuilderObject):
     class_ = Scrollbar
-    properties = ("orient",)
-    ro_properties = ("orient",)
+    properties = (
+        "orient",
+        "command",
+        "takefocus",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "variant",
+        # "surface_color",
+    )
+    ro_properties = properties
 
 
 register_widget(
@@ -374,10 +651,19 @@ register_widget(
 )
 
 
-class SeparatorBO(BuilderObject):
+class SeparatorBO(ttkbWidgetMixin, BuilderObject):
     class_ = Separator
-    properties = ("orient",)
-    ro_properties = ("orient",)
+    properties = (
+        "orient",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "variant",
+        # "surface_color",
+    )
+    ro_properties = properties
 
 
 register_widget(
@@ -389,9 +675,17 @@ register_widget(
 )
 
 
-class SizeGripBO(BuilderObject):
+class SizeGripBO(ttkbWidgetMixin, BuilderObject):
     class_ = SizeGrip
-    properties = []
+    properties = (
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "surface_color",
+    )
+    ro_properties = properties
 
 
 register_widget(
@@ -399,22 +693,34 @@ register_widget(
 )
 
 
-class SpinboxBO(BuilderObject):
+class SpinboxBO(ttkbWidgetMixin, BuilderObject):
     class_ = Spinbox
-    properties = ("from_", "to")
+    properties = (
+        "from_",
+        "to",
+        "increment",
+        "values",
+        "wrap",
+        "command",
+        "textvariable",
+        # "textsignal",  FIXME!
+        "format",
+        "width",
+        "state",
+        "takefocus",
+        "style",
+        "class_",
+        "cursor",
+        # "name",
+        # "color",
+        # "surface_color",
+    )
+    ro_properties = properties
+    command_properties = ("command",)
 
 
 register_widget(
     primitives.spinbox, SpinboxBO, "Spinbox", tab_group, group=primitives_ns.spinbox
-)
-
-
-class SwitchBO(CheckButtonBO):
-    class_ = Switch
-
-
-register_widget(
-    primitives.switch, SwitchBO, "Switch", tab_group, group=primitives_ns.switch
 )
 
 
