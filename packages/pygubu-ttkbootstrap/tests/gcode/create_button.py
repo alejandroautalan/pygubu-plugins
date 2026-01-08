dcbfecb9580ecf446a4fb67f37a5f324160d3514
Alejandro Autalan <alejandroautalan@gmail.com>
2026-01-08 00:27:46 -0300

    Add button test.

---
+++ packages/pygubu-ttkbootstrap/tests/gcode/create_button.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+import tkinter as tk
+from ttkbootstrap.widgets.primitives.button import Button
+
+
+def i18n_translator_noop(value):
+    """i18n - Setup translator in derived class file"""
+    return value
+
+
+def first_object_callback_noop(widget):
+    """on first objec callback - Setup callback in derived class file."""
+    pass
+
+
+def image_loader_default(master, image_name: str):
+    """Image loader - Setup image_loader in derived class file."""
+    return tk.PhotoImage(file=image_name, master=master)
+
+
+def find_callback(callbacks_bag, callback_uid):
+    cb = None
+
+    if isinstance(callbacks_bag, dict):
+        if callback_uid in callbacks_bag:
+            cb = callbacks_bag[callback_uid]
+    elif hasattr(callbacks_bag, callback_uid):
+        cb = getattr(callbacks_bag, callback_uid)
+    if cb is None:
+
+        def cb_undef(*args):
+            print(f"No function defined for {callback_uid}")
+
+        cb = cb_undef
+    return cb
+
+
+def create_button(
+    *,
+    master=None,
+    translator=None,
+    on_first_object_cb=None,
+    data_pool=None,
+    image_loader=None,
+    callbacks_bag=None,
+):
+    if translator is None:
+        translator = i18n_translator_noop
+    _ = translator  # i18n string marker.
+    if image_loader is None:
+        image_loader = image_loader_default
+    if on_first_object_cb is None:
+        on_first_object_cb = first_object_callback_noop
+
+    #
+    # Begin UI code
+    button1 = Button(
+        master,
+        text="button1",
+        icon="gear",
+        anchor="sw",
+        compound="right",
+        padding=5,
+        state="normal",
+        takefocus=True,
+    )
+    # First object created
+    on_first_object_cb(button1)
+
+    button1.pack(expand=True, ipadx=10, ipady=10, side="top")
+
+    return button1
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = create_button(root)
+    if isinstance(app, tk.Menu):
+        root.configure(menu=app)
+    root.mainloop()
---
+++ packages/pygubu-ttkbootstrap/tests/test_button.py
@@ -0,0 +1,35 @@
+import pygubu
+import unittest
+import ttkbootstrap as ttk
+
+from pathlib import Path
+from .support import get_root, TestWidget
+from .gcode.create_button import create_button
+
+
+UI_PATH = Path(__file__).parent / "ui" / "button.ui"
+
+
+#
+class TestLabel(TestWidget):
+    def create_widget(self):
+        self.widget = create_button(master=get_root())
+
+    def test_class(self):
+        self.assertIsInstance(self.widget, ttk.Button)
+        self.widget.destroy()
+
+
+class TestLabelBuilder(TestWidget):
+    def create_widget(self):
+        self.builder = pygubu.Builder()
+        self.builder.add_from_file(UI_PATH)
+        self.widget = self.builder.get_object("button1")
+
+    def test_class(self):
+        self.assertIsInstance(self.widget, ttk.Button)
+        self.widget.destroy()
+
+
+if __name__ == "__main__":
+    unittest.main()
